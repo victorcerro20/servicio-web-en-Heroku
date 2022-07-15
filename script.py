e0fede16322167e0bab14c6062cc29e86428c500
V1ct0rC3rr0 <victor.cerro20@gmail.com>
2022-07-15 00:50:45 -0400

    5

--- script.py
+++ script.py
@@ -15,7 +15,7 @@ def index():
     return flask.render_template('index.html')
 
 def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1, 4)
+    to_predict = np.array(to_predict_list).reshape(1, 5)
     loaded_model = pickle.load(open("checkpoints/victor.pkl","rb"))
     result = loaded_model.predict(to_predict)
     return result[0]
